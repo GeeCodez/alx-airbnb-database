c5d9f97e9096d0b51076c0144936486f14fa51fa
Gee Codez <godsway702@gmail.com>
2025-10-30 15:26:27 +0000

    using explain analyse to show the performance improvement of the queries after creating the indexes.

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,11 +1,14 @@
+--ANALYSIS BEFORE INDEXES
 EXPLAIN ANALYSE
 SELECT * FROM users WHERE role='admin';
 
+--CREATE INDEXS
 CREATE INDEX property_id_index ON property(property_id);
 CREATE INDEX property_id_b_index ON booking(property_Id);
 CREATE INDEX booking_id_index ON booking(booking_id);
 CREATE INDEX booking_id_p_index ON payment(booking_id);
 CREATE INDEX email_index ON users(email);
 
+--ANALYSIS AFTER INDEXES
 EXPLAIN ANALYSE
 SELECT * FROM users WHERE role='admin';
--- database-adv-script/index_performance.md
+++ database-adv-script/index_performance.md
@@ -1,5 +1,13 @@
 #Analysing performance using EXPLAIN ANALYSE
 
+# Performance before indexes
+Seq Scan on users  (cost=0.00..3.29 rows=11 width=90) (actual time=0.032..0.058 rows=11 loops=1)
+  Rows Removed by Filter: 92
+  Planning Time: 3.779 ms
+  Execution Time: 0.202 ms
+
+
+#PERFORMANCE ANALYSES AFTER INDEX
 Seq Scan on users  (cost=0.00..3.29 rows=11 width=90) (actual time=0.033..0.060 rows=11 loops=1)
 Filter: (role = 'admin'::user_role)
 Rows Removed by Filter: 92
