2d31b35b6f02af7aecb5db526ae99e326b4c2584
Gee Codez <godsway702@gmail.com>
2025-10-30 15:10:45 +0000

    updating the database index file

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,2 +1,11 @@
 EXPLAIN ANALYSE
-select * from users where role='admin';
+SELECT * FROM users WHERE role='admin';
+
+CREATE INDEX property_id_index ON property(property_id);
+CREATE INDEX property_id_b_index ON booking(property_Id);
+CREATE INDEX booking_id_index ON booking(booking_id);
+CREATE INDEX booking_id_p_index ON payment(booking_id);
+CREATE INDEX email_index ON users(email);
+
+EXPLAIN ANALYSE
+SELECT * FROM users WHERE role='admin';
