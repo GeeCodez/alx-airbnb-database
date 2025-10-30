3c3f826a616fab0c6b49074a73d8c1893c25307f
Gee Codez <godsway702@gmail.com>
2025-10-30 08:37:48 +0000

    aggregations_and_window_functions

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,9 +1,9 @@
 --query to find the number of booking by each user using the group by clause
-select u.first_name ||' '||u.last_name as username , count(b.booking_id) as total_bookings
-from users u join booking b on u.user_id=b.user_id 
-group by username;
+SELECT u.first_name ||' '||u.last_name AS username , COUNT(b.booking_id) as total_bookings
+FROM users u JOIN booking b ON u.user_id=b.user_id 
+GROUP BY username;
 
-select p.name, count(b.booking_id) as total_bookings, rank() OVER
-    (ORDER BY count(b.booking_id) DESC) as booking_rank
-    from property p join booking b on p.property_id=b.property_id
-    group by p.name;
+SELECT p.name, COUNT(b.booking_id) AS total_bookings, RANK() OVER
+    (ORDER BY count(b.booking_id) DESC) AS booking_rank
+    FROM property p JOIN booking b ON p.property_id=b.property_id
+    GROUP p.name;
